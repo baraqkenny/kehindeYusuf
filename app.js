664cd91e04e948722dd74413e782e7ba882ee674
Kehinde Mubaraq Yusuf <96214138+baraqkenny@users.noreply.github.com>
2022-08-28 10:06:56 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-let menu = document.getElementById('menu');
-let navbar2 = document.querySelector('.navbar2')
-let removeBtn = document.querySelector('.removeBtn')
+// let menu = document.getElementById('menu');
+// let navbar2 = document.querySelector('.navbar2')
+// let removeBtn = document.querySelector('.removeBtn')
 
 
 
-menu.addEventListener('click', ()=>{
-   navbar2.style.display = 'block';
-   menu.style.display = 'block'
-})
+// menu.addEventListener('click', ()=>{
+//    navbar2.style.display = 'block';
+//    menu.style.display = 'block'
+// })
 
-removeBtn.addEventListener('click', ()=>{
-    navbar2.style.display = 'none'
-})
+// removeBtn.addEventListener('click', ()=>{
+//     navbar2.style.display = 'none'
+// })
+
+function openSlideMenu(){
+    
+    document.querySelector('.side-nav').style.width = '250px'
+}
 
 
-
+function closeSlideMenu(){
+   document.querySelector('.side-nav').style.width = '0'
+  
+}
 
